de0d1979820e23c87fd34751a717d5acbfdcc344
Ginpei <ginpei@ginpei.dev>
2023-12-31 17:53:22 -0800

    set up Tailwind CSS

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,16 +1,23 @@
 import Head from "next/head";
+import { Container } from "../../lib/alignment/Container";
+import { VStack } from "../../lib/alignment/VStack";
 
 export interface HomePageProps {
 }
 
-export function HomePage({}: HomePageProps): JSX.Element {
+export function HomePage({ }: HomePageProps): JSX.Element {
   return (
     <div className="HomePage">
       <Head>
-        <title>HomePage</title>
+        <title>Home Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      HomePage
+      <Container>
+        <VStack>
+          <h1 className="text-4xl font-bold">Home Page</h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et maxime suscipit consectetur officia non in rerum quas aut beatae fuga debitis eligendi, nesciunt aperiam soluta aliquid rem ex vitae officiis.</p>
+        </VStack>
+      </Container>
     </div>
   );
 }
